ccc84890d6c963b8c9c29c6c39219d43ea5a2904
Administrador <ivan.24.burgio@gmail.com>
2023-10-05 20:16:13 -0300

    Estilos con tailwind al Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() { // rfc
   return (
     <>
-      <h1>Desde Header</h1>
-      <p>Hola mundo</p>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+        Seguimiento Pacientes {''}
+        <span className="text-indigo-600">Veterinaria</span>
+      </h1>
     </>
   )
 }
