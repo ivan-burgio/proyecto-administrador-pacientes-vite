bf1d53cd1d7c3eed7442332d7b017b40c802cd26
Administrador <ivan.24.burgio@gmail.com>
2023-10-09 17:51:14 -0300

    generando un id unico para cada paciente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,6 +10,13 @@ export default function Formulario({pacientes, setPacientes}) {
 
     const [error, setError] = useState(false);
 
+    const generarId = () => {
+      const random = Math.random().toString(36).substr(2);
+      const fecha = Date.now().toString(36);
+
+      return random + fecha;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -28,7 +35,8 @@ export default function Formulario({pacientes, setPacientes}) {
             propietario,
             email,
             fecha,
-            sintomas
+            sintomas,
+            id: generarId()
         }
 
         setPacientes([...pacientes, objetoPaciente]);
