7be5302e982ef567df3418356db7a487bbabd341
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-16 18:58:54 -0300

    Fin Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,8 @@ import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 export default function App() {
-    const [pacientes, setPacientes] = useState([]);
+    const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
     const [paciente, setPaciente] = useState({});
-
-    useEffect(() => {
-        const obtenerLS = () => {
-            const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
-            setPacientes(pacientesLS);
-        }
-
-        obtenerLS();
-    }, [])
 
     useEffect(() => {
         localStorage.setItem('pacientes', JSON.stringify(pacientes));
