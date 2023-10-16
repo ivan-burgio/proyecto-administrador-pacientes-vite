ab797fb65473847bea989d9247694b7a2aa2d0ca
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-16 15:31:42 -0300

    Funcion de eliminar andando, guardar lso apcientes en localstorage no funca

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -6,6 +6,25 @@ import ListadoPacientes from "./components/ListadoPacientes"
 export default function App() {
     const [pacientes, setPacientes] = useState([]);
     const [paciente, setPaciente] = useState({});
+
+    useEffect(() => {
+        const obtenerLS = () => {
+            const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+            setPacientes(pacientesLS);
+        }
+
+        obtenerLS();
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('pacientes', JSON.stringify(pacientes));
+    }, [pacientes])
+
+    const eliminarPaciente = id => {
+        const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+
+        setPacientes(pacientesActualizados);
+    }
 
     return (
         <div className="container mx-auto mt-10">
@@ -20,6 +39,7 @@ export default function App() {
                 <ListadoPacientes
                     pacientes = {pacientes}
                     setPaciente = {setPaciente}
+                    eliminarPaciente = {eliminarPaciente}
                 />
             </div>
         </div>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,13 @@
-export default function Paciente({paciente, setPaciente}) {
-    const {nombre, propietario, email, fecha, sintomas} = paciente;
+export default function Paciente({paciente, setPaciente, eliminarPaciente}) {
+    const {nombre, propietario, email, fecha, sintomas, id} = paciente;
+
+    const handleEliminar = () => {
+        const respuesta = confirm('Se eliminara el paciente');
+
+        if(respuesta) {
+            eliminarPaciente(id);
+        }
+    }
 
     return (
         <div className="m-5 bg-white shadow-md px-5 py-10 rounded-xl">
@@ -37,6 +45,7 @@ export default function Paciente({paciente, setPaciente}) {
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white font-bold uppercase rounded-lg"
+                    onClick={handleEliminar}
                 >Eliminar</button>
             </div>
         </div>
